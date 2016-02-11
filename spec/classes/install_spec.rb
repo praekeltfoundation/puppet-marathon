3c504f7931fc9eeaa8b5d975b5944ced2bb8544c
Jamie Hewland <jhewland@gmail.com>
2016-02-11 12:27:20 +0200

    Add tests for install class

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe 'marathon::install' do
-  it { is_expected.to compile }
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
+
+      it { is_expected.to compile }
+
+      describe 'with default options' do
+        it do
+          is_expected.to contain_package('marathon').with_ensure('installed')
+        end
+      end
+
+      describe 'when ensure is purged' do
+        let(:params) { {:ensure => 'purged'} }
+        it do
+          is_expected.to contain_package('marathon').with_ensure('purged')
+        end
+      end
+    end
+  end
 end
